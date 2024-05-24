e3bca345fd68e45f2c635d2b03ffc6f2ba894d5c
Fe24le <legasy23dream@gmail.com>
2024-05-24 20:09:48 -0300

    v1

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-// index.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const axios = require('axios');
+server.use(middlewares);
+server.use(router);
 
-// Función para consultar los datos de usuarios
-async function consultarUsuarios() {
-  try {
-    const response = await axios.get('http://localhost:3000/peliculas');
-    const usuarios = response.data;
-    console.log('Datos de usuarios:', usuarios);
-    // Aquí puedes hacer lo que quieras con los datos, como renderizarlos en tu página web
-  } catch (error) {
-    console.error('Error al obtener datos de usuarios:', error);
-  }
-}
+server.listen(port, () => {
+  console.log(`JSON Server está corriendo en el puerto  ${port}`);
+});
 
-// Llamar a la función para consultar usuarios
-consultarUsuarios();
+
+
+
+
+
+// // index.js
+
+// const axios = require('axios');
+
+// // Función para consultar los datos de usuarios
+// async function consultarUsuarios() {
+//   try {
+//     const response = await axios.get('http://localhost:3000/peliculas');
+//     const usuarios = response.data;
+//     console.log('Datos de usuarios:', usuarios);
+//     // Aquí puedes hacer lo que quieras con los datos, como renderizarlos en tu página web
+//   } catch (error) {
+//     console.error('Error al obtener datos de usuarios:', error);
+//   }
+// }
+
+// // Llamar a la función para consultar usuarios
+// consultarUsuarios();
